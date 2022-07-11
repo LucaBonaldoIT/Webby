b053b363931ea8e8aeb573a624286a5b22dbada2
LucaBonaldoIT <lucabonaldoit@gmail.com>
2022-07-11 17:18:47 +0200

    New UI

--- js/right-click-menu.js
+++ js/right-click-menu.js
@@ -1,1 +1,43 @@
-$('document').on('right-')
+is_menu_open = false;
+
+$(document).on("contextmenu", function (e) {
+  e.preventDefault();
+
+  if (!is_menu_open) {
+    $(".right-click-menu")
+      .css("left", e.pageX + "px")
+      .css("top", e.pageY + "px")
+      .css("pointer-events", "all")
+      .css("z-index", "2")
+      .css("opacity", "1");
+
+    is_menu_open = true;
+  } else {
+    $(".right-click-menu")
+      .css("pointer-events", "none")
+      .css("z-index", "0")
+      .css("opacity", "0");
+    is_menu_open = false;
+  }
+});
+
+$(document).on("click", function (e) {
+  if (!is_menu_open) return;
+
+  $(".right-click-menu")
+    .css("pointer-events", "none")
+    .css("z-index", "0")
+    .css("opacity", "0");
+  is_menu_open = false;
+  e.preventDefault();
+});
+
+$(document).on("click", ".right-click-menu", function (e) {
+  if (!is_menu_open) return;
+
+  $(".right-click-menu")
+    .css("pointer-events", "none")
+    .css("z-index", "0")
+    .css("opacity", "0");
+  is_menu_open = false;
+});
